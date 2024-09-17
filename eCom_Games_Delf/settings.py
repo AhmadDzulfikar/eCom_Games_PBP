e48cf8fd828cb083e58710499491efe4c4bc34cb
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2024-09-17 15:10:20 +0700

    Tugas3 Mungkin Selesai

--- eCom_Games_Delf/settings.py
+++ eCom_Games_Delf/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'eCom_Games_Delf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
