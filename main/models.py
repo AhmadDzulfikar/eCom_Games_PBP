a5ef2b5db88c69fdd401faaddbb1cfa8b2f42ea1
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2024-09-24 20:32:33 +0700

    mencoba menyelsaikan tugas 4

--- main/models.py
+++ main/models.py
@@ -1,10 +1,13 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
